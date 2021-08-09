b058e169ca8cfeefdb9a8299704e5716410735f0
Edigleysson Silva <edigleyssonsilva@gmail.com>
2021-08-09 16:20:29 -0300

    feat: added javascript demos

---
+++ javascript/basic-modules/main.js
@@ -0,0 +1,4 @@
+// import { showHello, name } from './modules/module1.js';
+
+// showHello();
+// console.log(name);
---
+++ javascript/basic-modules/modules/module1.js
@@ -0,0 +1,5 @@
+export const name = 'module1';
+
+export function showHello() {
+  alert('hello world');
+}
